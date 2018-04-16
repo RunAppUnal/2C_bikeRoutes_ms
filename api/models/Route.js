9b628067144e057af6dc42708ddee6f2c4f75fe7
Jesus Felipe Chavarro Muñoz <jfchavarrom@unal.edu.co>
2018-04-16 02:01:06 -0500

    Origen and destination names with geocoding and rute match fix

--- api/models/Route.js
+++ api/models/Route.js
@@ -44,6 +44,14 @@ module.exports = {
       required: true
     },
 
+    originAddr : {
+      type: 'string'
+    },
+
+    destinationAddr : {
+      type: 'string'
+    },
+
     route_points : { 
       type: 'json'//LineString GeoJSON
      },
@@ -56,41 +64,41 @@ module.exports = {
       var route = route || this;
       Route.native(function (error, collection) {
         if (error) return callback(error);
-        let a = new Date(new Date(route.time).setMinutes(route.time.getMinutes()-5));
-        let b = new Date(new Date(route.time).setMinutes(route.time.getMinutes()+5))
+        let a = new Date(new Date(route.time).setMinutes(route.time.getMinutes()-10));
+        let b = new Date(new Date(route.time).setMinutes(route.time.getMinutes()+10))
         collection.find({
           time: { $gt: a, $lt: b},
           origin: {
             $geoWithin:{
               $centerSphere:[
                 route.origin, //circle center
-                2/6378.1//1.4 kilometer in radians - circle radius
+                2/6378.1//2 kilometer in radians - circle radius
               ]
             }
           }
         }).toArray(function (err, results) {
-          //console.log("Mongo results: %O", results);
+          console.debug("Mongo results: %O", results);
           
           if (err) return callback(err);
           let r = results.filter((val) => {
-            if(val.route_points){             
+            let length = lengthLine({
+              type: 'LineString',
+              coordinates: [
+                val.destination, route.destination
+              ]
+            })
+            console.info("destination length with userID %o: %o\n",val.user_id, length);
+            if(length < 5){
+              return true
+            }else if(val.route_points){             
               let overlap = lineOverlap(route.route_points, val.route_points,{tolerance:0.1});
-
+              console.info("overlap with routeID %o: %o\n",val._id, "overlap");
               let length = overlap.features.reduce(function (sum,path) {
                 return sum + lengthLine(path.geometry);  
               },0)
-              //console.log("length with userID %o:\n%o\n",val.user_id, length);
+              console.info("length with userID %o: %o\n",val.user_id, length);
               return length > 2
-            }else{
-              let length = lengthLine({
-                type: 'LineString',
-                coordinates: [
-                  val.destination, route.destination
-                ]
-              })
-              //console.log("length with userID %o:\n%o\n",val.user_id, length);
-              return length < 5
-            }
+            }           
           })
           
           route.similar_routes = r;
@@ -103,10 +111,11 @@ module.exports = {
   },
     
   beforeCreate: function (values, cb) {
-    
+    var origin = values.origin.slice().reverse();
+    var destination = values.destination.slice().reverse();
     googleMapsClient.directions({
-      origin: values.origin.slice().reverse(),
-      destination: values.destination.slice().reverse(),
+      origin: origin,
+      destination: destination,
       mode: "walking",
       region: "co",
       units: "metric"
@@ -124,11 +133,41 @@ module.exports = {
       console.error("Error in google directions request: ", err);
       cb()
     })
+
+
+    googleMapsClient.reverseGeocode({
+      'latlng': origin.toString()
+    }, function(err, response) {
+      if (!err) {
+        if (response && response.json && response.json.results[1]){
+          values.originAddr = response.json.results[1].formatted_address.replace(/, (?:Bogot.|Colombia)/gi,'');
+          console.log("OriginAddr: ",values.originAddr);
+        }
+        else {
+          console.log('No results found - originAddr');
+        }
+      } else {
+        console.log('originAddr - Geocoder failed due to: ' + status);
+      }
+    });
+
+    googleMapsClient.reverseGeocode({
+      'latlng': destination.toString()
+    }, function(err, response) {
+      if (!err) {
+        if (response && response.json && response.json.results[1]){
+          values.destinationAddr = response.json.results[1].formatted_address.replace(/, (?:Bogot.|Colombia)/gi,'');
+          console.log("destinationAddr: ",values.destinationAddr);
+        }
+        else {
+          console.log('No results found - destinationAddr');
+        }
+      } else {
+        console.log('destinationAddr - Geocoder failed due to:',err);
+      }
+    });
     
   }
-
-  
-
 
 };
 
